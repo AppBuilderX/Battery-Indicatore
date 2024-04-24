0ec3ca6f26338761a3179c76f94ebc6d1cd09812
LENOVO <anujsinghcodes@gmail.com>
2024-04-24 13:27:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/batteryindicatore/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.batteryindicatore;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+    BroadcastReceiver broadcastReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView);
+        progressBar = findViewById(R.id.progressBar);
+
+        batteryLevel();
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(broadcastReceiver, intentFilter);
+
+    }
+
+    private void batteryLevel(){
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                textView.setText("Battery level remaining : " + Integer.toString(level)+"%");
+                progressBar.setProgress(level);
+            }
+        };
+    }
+
+}
